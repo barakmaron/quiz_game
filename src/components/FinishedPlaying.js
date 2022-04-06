9965a3ba819b7b947052c1de48b0302daf085e0e
Barak Maron <Barak062@gmail.com>
2022-04-06 14:03:16 +0300

    Add sorted final score

--- src/components/FinishedPlaying.js
+++ src/components/FinishedPlaying.js
@@ -1,4 +1,4 @@
-import { React, useState} from 'react'
+import { React, useState, useEffect} from 'react'
 import { FaStar, FaUserAstronaut } from 'react-icons/fa';
 import { GiTrophy } from 'react-icons/gi';
 import Button from './Button';
@@ -9,9 +9,27 @@ const FinishedPlaying = ({players, score}) => {
     let place = '';
     const color = ['2eb952', '35afe9', 'bd35e9', 'e9a135', '8bb722', 'e93535', '355ce9'];
     const placeColor = ['gold', 'silver', '#CD7F32'];
+    const [playersObj, setPlayersObj] = useState([{name: '-1', score: 0}]);
+    const sortScoresToPlyersObjects = (players, score) =>
+    {
+        players.forEach((player, i) => {
+            setPlayersObj(s => {
+                if(s[0].name === '-1')
+                    return[{name: player.value, score: score[i]}];
+                return[...s, {name: player.value, score: score[i]}];
+            });
+        });
+        playersObj.sort((a, b) => a.score - b.score);
+    };
+    useEffect(() => {
+        console.log(players)
+        if(playersObj[0].name === '-1')
+            sortScoresToPlyersObjects(players, score);
+    }, [players, score]);
+    
   return (<>
     <div className='finishedPlaying'>        
-      {score.map((playerScore, i) =>
+      {playersObj.map((player, i) =>
       {
         if(i < 3)
         {
@@ -23,8 +41,8 @@ const FinishedPlaying = ({players, score}) => {
         }
         return(<div key={i} className='playerStats' style={{color: '#' + color[i % 7]}}>
             {place}
-            <h3><FaUserAstronaut style={{color: '#009fff'}}></FaUserAstronaut> {players[i].value}</h3>
-            <span><FaStar style={{color: 'gold'}}></FaStar> {playerScore}</span>            
+            <h3><FaUserAstronaut style={{color: '#009fff'}}></FaUserAstronaut> {player.name}</h3>
+            <span><FaStar style={{color: 'gold'}}></FaStar> {player.score}</span>            
         </div>)
       })}       
     </div>    
