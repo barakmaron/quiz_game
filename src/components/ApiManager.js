178188a990d897f258399b184d12b612768910c0
Barak Maron <Barak062@gmail.com>
2022-04-06 13:27:04 +0300

    Rand for questions

--- src/components/ApiManager.js
+++ src/components/ApiManager.js
@@ -1,3 +1,5 @@
+import {shuffleArray} from './Utilities.js';
+
 export default async function  GetCategories()
 {
     const res = await fetch('https://opentdb.com/api_category.php');
@@ -7,8 +9,9 @@ export default async function  GetCategories()
 
 export async function GetQuestions(difficulty, category, numPlayers)
 {
-    const numQuestions = numPlayers * 5;
+    const numQuestions = numPlayers * 5 * 100;
     const res = await fetch('https://opentdb.com/api.php?amount=' + numQuestions + '&category=' + category +'&difficulty=' + difficulty + '&type=multiple&encode=base64');
     const data = await res.json();
-    return data;
+    const randData = shuffleArray(data);
+    return randData;
 }
