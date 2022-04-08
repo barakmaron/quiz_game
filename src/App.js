80dc45a0f1974701e6df67d9ca7b7e911784e5db
Barak Maron <Barak062@gmail.com>
2022-04-08 12:33:46 +0300

    Configure React app for deployment to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { React}  from 'react';
-import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Navigate} from "react-router-dom";
 
 import './App.css';
 
@@ -15,6 +15,17 @@ function App() {
         <Header></Header> 
         <div className='mainContent'>
           <Routes>
+            <Route path='/' element={<Navigate replace to="/intro" />} />
+            <Route path='/intro' element={
+              <div className='intro'>
+                <h2>INTRO</h2>
+                <p>This is my first project built in react, it was a great experience building, testing and learning.
+                  Hope you will have fun playing the game as much as i had building it.
+                </p>
+                <br></br>
+                <p>The Server (backend) is a very simple php script that manages posting scores and name to the database as well as getting the TOP 10 players from the database.</p>
+              </div>
+            }/>
             <Route path='/game' element={<Game></Game>} />
             <Route path='/leaderboard' element={<Leaderborad></Leaderborad>} />
           </Routes>
