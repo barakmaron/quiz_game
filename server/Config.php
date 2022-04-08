595db4e593c616c4cbb5d1b7fa50f2032f3bbb25
Barak Maron <Barak062@gmail.com>
2022-04-08 20:59:24 +0300

    fix bug 2

--- server/Config.php
+++ server/Config.php
@@ -2,7 +2,7 @@
      ini_set('display_errors', 'on');     
      date_default_timezone_set("Asia/Jerusalem");
      header("Content-Type: text/html; charset=utf-8");
-     $mysqli = new mysqli("127.0.0.1", 'root', '48625');
+     $mysqli = new mysqli("127.0.0.1", 'QuizGameProject', '@%)je/}G@lx1s!UB');
      $mysqli->set_charset('utf8');
      if($mysqli == false)
         die("ERROR: Could not connect. " . mysqli_connect_error());
